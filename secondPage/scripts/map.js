a54805e55bfd1953dd4ab35f0a9b00cf36981115
S_tyler <islavinskyi@gmail.com>
2017-02-07 17:42:12 +0200

    new 2

--- secondPage/scripts/map.js
+++ secondPage/scripts/map.js
@@ -1,11 +1,17 @@
 function init() {
     var mapDiv = document.getElementById("mymap");
+    var myLatLng = {lat: 50.4685586, lng: 30.5167358};
     var mapOptions = {
-        center: new google.maps.LatLng(50.27, 30.31),
-        zoom: 14,
+        center: new google.maps.LatLng(myLatLng),
+        zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(mapDiv, mapOptions);  
+    var map = new google.maps.Map(mapDiv, mapOptions);
+    var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });  
     
 }
 window.onload = init;
